e4930a82106f2ebaeb8af7ca695d6a626d57afbc
Vanshika3101 <vanshikanarang143@gmail.com>
2026-04-22 21:32:11 +0530

    Update Movie.js

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -4,8 +4,6 @@ const movieSchema = new mongoose.Schema({
     title: String,
     description: String,
     posterUrl: String,
-    duration: Number,
-    language: String
 });
 
 module.exports = mongoose.model("Movie", movieSchema);
